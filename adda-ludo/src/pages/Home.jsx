82f615d5a45ea4fc08809236910da9b6e3c3039d
Sagar Saini <sagarsaini8003656@gmail.com>
2026-07-05 22:43:33 +0530

    Update Home.jsx

--- adda-ludo/src/pages/Home.jsx
+++ adda-ludo/src/pages/Home.jsx
@@ -22,9 +22,9 @@ export default function Home() {
       title: "Welcome",
       subtitle: "Hot games • Instant wins • Safe play",
       heading: "Refer & Earn",
-      text: "👉25 लोगो को Refer करने पर ₹1100 बोनस💰Free",
-      text: "👉50 लोगो को Refer करने पर ₹2100 बोनस💰Free",
-      text: "👉100 लोगो को Refer करने पर ₹5100 बोनस💰Free",
+      text: "👉25 लोगो को Refer करने पर ₹1100 बोनस💰Free
+             👉50 लोगो को Refer करने पर ₹2100 बोनस💰Free
+             👉100 लोगो को Refer करने पर ₹5100 बोनस💰Free",
       button: "Refer Now",
       route: "/refer",
       tag: "REFER BONUS",
