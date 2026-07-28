cb85382b7b5624a4e6ee09e1f7efd7f393149002
Sagar Saini <sagarsaini8003656@gmail.com>
2026-07-28 18:50:48 +0530

    Update support contact number in Support.jsx

--- adda-ludo/src/pages/Support.jsx
+++ adda-ludo/src/pages/Support.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 export default function Support() {
   const navigate = useNavigate();
 
-  const supportNumber = "917737005131";
+  const supportNumber = "919983776947";
   const whatsappLink = `https://api.whatsapp.com/send?phone=${supportNumber}&text=Hello%20Support`;
 
   return (
